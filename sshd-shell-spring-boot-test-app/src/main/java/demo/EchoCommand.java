d4896bc01fa1c6b2f53b25dde63d27dfb0f661ff
anand <anand1st@gmail.com>
2017-06-16 19:16:34 +0800

    Added example for SshSessionContext.readInput

--- sshd-shell-spring-boot-test-app/src/main/java/demo/EchoCommand.java
+++ sshd-shell-spring-boot-test-app/src/main/java/demo/EchoCommand.java
@@ -1,6 +1,8 @@
 package demo;
 
+import java.io.IOException;
 import org.springframework.stereotype.Component;
+import sshd.shell.springboot.autoconfiguration.SshSessionContext;
 import sshd.shell.springboot.autoconfiguration.SshdShellCommand;
 
 /**
@@ -12,8 +14,8 @@ import sshd.shell.springboot.autoconfiguration.SshdShellCommand;
 public class EchoCommand {
     
     @SshdShellCommand(value = "bob", description = "Bob's echo. Usage: echo bob <arg>")
-    public String bobSays(String arg) {
-        return "bob says " + arg;
+    public String bobSays(String arg) throws IOException {
+        return "bob echoes " + arg + " and hello " + SshSessionContext.readInput("What's your name?");
     }
     
     @SshdShellCommand(value = "alice", description = "Alice's echo. Usage: echo alice <arg>")
