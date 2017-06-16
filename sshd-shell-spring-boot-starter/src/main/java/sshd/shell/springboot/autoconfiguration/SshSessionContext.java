f1d9ab8fb694f83e9c9da0836fc1ffdc81a7f0c6
anand <anand1st@gmail.com>
2017-06-16 18:13:27 +0800

    Added SshSessionContext to hold information inside a session and also for interactive IO support

---
+++ sshd-shell-spring-boot-starter/src/main/java/sshd/shell/springboot/autoconfiguration/SshSessionContext.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 anand.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sshd.shell.springboot.autoconfiguration;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import jline.console.ConsoleReader;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
+
+/**
+ *
+ * @author anand
+ */
+public enum SshSessionContext {
+
+    ;
+
+    private static final ThreadLocal<Map<String, Object>> THREAD_CONTEXT = new ThreadLocal<Map<String, Object>>() {
+        @Override
+        protected Map<String, Object> initialValue() {
+            return new HashMap<>();
+        }
+    };
+    
+    static final String CONSOLE_READER = "__consoleReader";
+    static final String TEXT_COLOR = "__textColor";
+
+    public static void put(String key, Object value) {
+        THREAD_CONTEXT.get().put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E> E get(String key) {
+        return (E) THREAD_CONTEXT.get().get(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E> E remove(String key) {
+        return (E) THREAD_CONTEXT.get().remove(key);
+    }
+    
+    public static boolean containsKey(String key) {
+        return THREAD_CONTEXT.get().containsKey(key);
+    }
+
+    public static void clear() {
+        THREAD_CONTEXT.remove();
+    }
+    
+    public static String readLine(String text, Character mask) throws IOException {
+        ConsoleReader reader = get(CONSOLE_READER);
+        AnsiColor textColor = get(TEXT_COLOR);
+        return reader.readLine(AnsiOutput.encode(textColor) + text + " " + AnsiOutput.encode(AnsiColor.DEFAULT), mask);
+    }
+    
+    public static String readLine(String text) throws IOException {
+        return readLine(text, null);
+    }
+}
