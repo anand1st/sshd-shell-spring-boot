3e002da1cc3ef2d659c7cca4232485d217f58274
anand <anand1st@gmail.com>
2017-10-06 09:49:12 +0800

    Processor to return outputs with highlighted text according to search

---
+++ sshd-shell-spring-boot-starter/src/main/java/sshd/shell/springboot/console/HighlightUserInputProcessor.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 anand.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sshd.shell.springboot.console;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import sshd.shell.springboot.console.UsageInfo.Row;
+
+/**
+ *
+ * @author anand
+ */
+@Component
+@Order(1)
+class HighlightUserInputProcessor extends BaseUserInputProcessor {
+
+    private final Pattern pattern = Pattern.compile("[\\w\\W]+\\s?\\|\\s?h (.+)");
+
+    @Override
+    public Optional<UsageInfo> getUsageInfo() {
+        return Optional.of(new UsageInfo(Arrays.<Row>asList(
+                new Row("h <arg>", "Highlights <arg> in response output of command execution"),
+                new Row("", "Example usage: help | h exit"))));
+    }
+
+    @Override
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    @Override
+    public void processUserInput(String userInput) throws InterruptedException, ShellException {
+        String[] part = splitAndValidateCommand(userInput, "\\|", 2);
+        Matcher matcher = pattern.matcher(userInput);
+        Assert.isTrue(matcher.find(), "Unexpected error");
+        String textToHighlight = matcher.group(1).trim();
+        String output = processCommands(part[0]);
+        ConsoleIO.writeOutput(output, textToHighlight);
+    }
+}
