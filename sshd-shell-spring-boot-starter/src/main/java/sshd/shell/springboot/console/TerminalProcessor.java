d0563668ba7f53a845c12caf7684455329d375b4
anand <anand1st@gmail.com>
2017-10-06 09:51:59 +0800

    Refactored to use BaseUserInputProcessor according to regex of user input

--- sshd-shell-spring-boot-starter/src/main/java/sshd/shell/springboot/console/TerminalProcessor.java
+++ sshd-shell-spring-boot-starter/src/main/java/sshd/shell/springboot/console/TerminalProcessor.java
@@ -18,9 +18,8 @@ package sshd.shell.springboot.console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
+import java.util.List;
+import java.util.regex.Matcher;
 import org.jline.reader.Completer;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
@@ -30,7 +29,6 @@ import org.jline.terminal.TerminalBuilder;
 import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 import sshd.shell.springboot.autoconfiguration.ColorType;
-import sshd.shell.springboot.autoconfiguration.CommandExecutableDetails;
 import sshd.shell.springboot.autoconfiguration.Constants;
 import sshd.shell.springboot.autoconfiguration.SshSessionContext;
 import sshd.shell.springboot.autoconfiguration.SshdShellProperties;
@@ -45,11 +43,11 @@ public class TerminalProcessor {
 
     private static final String SUPPORTED_COMMANDS_MESSAGE = "Enter '" + Constants.HELP
             + "' for a list of supported commands";
-    private static final String UNSUPPORTED_COMMANDS_MESSAGE = "Unknown command. " + SUPPORTED_COMMANDS_MESSAGE;
+    static final String UNSUPPORTED_COMMANDS_MESSAGE = "Unknown command. " + SUPPORTED_COMMANDS_MESSAGE;
 
     private final SshdShellProperties.Shell properties;
-    private final Map<String, Map<String, CommandExecutableDetails>> commandMap;
     private final Completer completer;
+    private final List<BaseUserInputProcessor> userInputProcessors;
 
     public void processInputs(InputStream is, OutputStream os, String terminalType) {
         try (Terminal terminal = TerminalBuilder.builder().system(false).type(terminalType).streams(is, os).build()) {
@@ -67,6 +65,8 @@ public class TerminalProcessor {
     private void createDefaultSessionContext(LineReader reader, Terminal terminal) {
         SshSessionContext.put(ConsoleIO.LINE_READER, reader);
         SshSessionContext.put(ConsoleIO.TEXT_STYLE, getStyle(properties.getText().getColor()));
+        SshSessionContext.put(ConsoleIO.HIGHLIGHT_COLOR,
+                AttributedStyle.DEFAULT.background(properties.getText().getHighlightColor().value));
         SshSessionContext.put(ConsoleIO.TERMINAL, terminal);
     }
 
@@ -95,72 +95,14 @@ public class TerminalProcessor {
 
     private void handleUserInput(String userInput) throws InterruptedException, ShellException {
         if (!userInput.isEmpty()) {
-            String[] part = userInput.split(" ", 3); // Three parts: command, subcommand, arg
-            Collection<String> userRoles = getUserRoles(part[0]);
-            if (part.length < 2) {
-                handleSingleTokenUserInput(part[0], userRoles);
-            } else {
-                handleUserInputWithMoreTokens(part, userRoles);
+            for (BaseUserInputProcessor userInputProcessor : userInputProcessors) {
+                Matcher matcher = userInputProcessor.getPattern().matcher(userInput);
+                if (matcher.matches()) {
+                    userInputProcessor.processUserInput(userInput);
+                    return;
+                }
             }
-        }
-    }
-
-    private Collection<String> getUserRoles(String command) throws ShellException {
-        Collection<String> userRoles = SshSessionContext.<Collection<String>>get(Constants.USER_ROLES);
-        Map<String, CommandExecutableDetails> commandExecutables = getCommandExecutables(command);
-        CommandExecutableDetails ced = commandExecutables.get(Constants.EXECUTE);
-        validateExecutableWithUserRole(ced, userRoles);
-        return userRoles;
-    }
-
-    private Map<String, CommandExecutableDetails> getCommandExecutables(String command) throws ShellException {
-        Map<String, CommandExecutableDetails> commandExecutables = commandMap.get(command);
-        if (Objects.isNull(commandExecutables)) {
-            throw new ShellException(UNSUPPORTED_COMMANDS_MESSAGE);
-        }
-        return commandExecutables;
-    }
-
-    private void validateExecutableWithUserRole(CommandExecutableDetails ced, Collection<String> userRoles) throws
-            ShellException {
-        if (!ced.matchesRole(userRoles)) {
-            throw new ShellException("Permission denied");
-        }
-    }
-
-    private void handleSingleTokenUserInput(String command, Collection<String> userRoles) throws InterruptedException {
-        CommandExecutableDetails ced = commandMap.get(command).get(Constants.EXECUTE);
-        ConsoleIO.writeOutput(Objects.isNull(ced.getCommandExecutor()) ? unknownSubcommandMessage(command, userRoles)
-                : ced.executeWithArg(null));
-    }
-
-    private String unknownSubcommandMessage(String command, Collection<String> userRoles) {
-        StringBuilder sb = new StringBuilder("Supported subcommand for ").append(command);
-        commandMap.get(command).entrySet().stream()
-                .filter(e -> !e.getKey().equals(Constants.EXECUTE) && e.getValue().matchesRole(userRoles))
-                .forEach(e -> sb.append("\n\r").append(e.getKey()).append("\t\t")
-                .append(e.getValue().getDescription()));
-        return sb.toString();
-    }
-
-    private void handleUserInputWithMoreTokens(String[] part, Collection<String> userRoles) throws
-            InterruptedException, ShellException {
-        Map<String, CommandExecutableDetails> commandExecutables = commandMap.get(part[0]);
-        if (!commandExecutables.containsKey(part[1])) {
-            throw new ShellException("Unknown subcommand '" + part[1] + "'. Type '" + part[0]
-                    + "' for supported subcommands");
-        }
-        CommandExecutableDetails ced = commandMap.get(part[0]).get(part[1]);
-        validateExecutableWithUserRole(ced, userRoles);
-        ConsoleIO.writeOutput(ced.executeWithArg(part.length == 2 ? null : part[2]));
-    }
-
-    private static class ShellException extends Exception {
-
-        private static final long serialVersionUID = 7114130906989289480L;
-
-        public ShellException(String message) {
-            super(message);
+            throw new ShellException("Unsupported command post processor! Should not happen");
         }
     }
 }
