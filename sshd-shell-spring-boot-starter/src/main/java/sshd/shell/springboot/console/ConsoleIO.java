2d99404a6668340d32c93109fdf7d8fc9d81215b
anand <anand1st@gmail.com>
2017-10-23 15:13:53 +0800

    Added functionality to write as json

--- sshd-shell-spring-boot-starter/src/main/java/sshd/shell/springboot/console/ConsoleIO.java
+++ sshd-shell-spring-boot-starter/src/main/java/sshd/shell/springboot/console/ConsoleIO.java
@@ -15,6 +15,9 @@
  */
 package sshd.shell.springboot.console;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import java.io.IOException;
 import java.util.Objects;
 import org.jline.reader.LineReader;
@@ -27,9 +30,11 @@ import sshd.shell.springboot.autoconfiguration.SshSessionContext;
  *
  * @author anand
  */
+@lombok.extern.slf4j.Slf4j
 public enum ConsoleIO {
     ;
-        
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final ObjectWriter WRITER = OBJECT_MAPPER.writer().withDefaultPrettyPrinter();
     static final String LINE_READER = "__lineReader";
     static final String TEXT_STYLE = "__textStyle";
     static final String TERMINAL = "__terminal";
@@ -72,6 +77,12 @@ public enum ConsoleIO {
         writeOutput(output, null);
     }
 
+    /**
+     * Write highlighted output.
+     *
+     * @param output Text output
+     * @param textToHighlight text to highlight
+     */
     public static void writeOutput(String output, String textToHighlight) {
         Terminal terminal = SshSessionContext.<Terminal>get(TERMINAL);
         AttributedStyle textStyle = SshSessionContext.<AttributedStyle>get(TEXT_STYLE);
@@ -88,5 +99,26 @@ public enum ConsoleIO {
         }
         terminal.writer().println(builder.style(AttributedStyle.DEFAULT).toAnsi(terminal));
         terminal.flush();
+    }
+
+    public static void writeJsonOutput(Object object) {
+        writeJsonOutput(object, null);
+    }
+
+    public static void writeJsonOutput(Object object, String textToHighlight) {
+        writeOutput(asJson(object), textToHighlight);
+    }
+
+    public static String asJson(Object object) {
+        try {
+            return WRITER.writeValueAsString(object);
+        } catch (JsonProcessingException ex) {
+            log.error("Error processing json output", ex);
+            return "Error processing json output: " + ex.getMessage();
+        }
+    }
+
+    public static <E> E stringToObject(String json, Class<E> clazz) throws IOException {
+        return OBJECT_MAPPER.readValue(json, clazz);
     }
 }
