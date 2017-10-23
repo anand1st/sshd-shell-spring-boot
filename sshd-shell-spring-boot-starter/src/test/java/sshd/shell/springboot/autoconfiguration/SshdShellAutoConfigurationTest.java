97b345b21172948010bfd1fcaf0158334b6937f3
anand <anand1st@gmail.com>
2017-10-23 15:14:13 +0800

    Updated test cases

--- sshd-shell-spring-boot-starter/src/test/java/sshd/shell/springboot/autoconfiguration/SshdShellAutoConfigurationTest.java
+++ sshd-shell-spring-boot-starter/src/test/java/sshd/shell/springboot/autoconfiguration/SshdShellAutoConfigurationTest.java
@@ -34,6 +34,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.util.SocketUtils;
 
 /**
@@ -54,7 +55,7 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
             verifyResponse(is, "Exiting shell");
         });
     }
-    
+
     // FIXME Figure out why following test case fails with command line call but passes with IDE
     @Ignore
     @Test
@@ -101,21 +102,27 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
     @Test
     public void testHelp() throws JSchException, IOException {
         sshCall((is, os) -> {
+            String format = "\r\n%-35s%s"; // must be same with usageInfoFormat in SshdShellProperties.java
             write(os, "help");
-            StringBuilder format = new StringBuilder("Supported Commands");
-            for (int i = 0; i < 6; i++) {
-                format.append("\r\n%-20s%s");
+            StringBuilder sb = new StringBuilder("Supported Commands");
+            for (int i = 0; i < 19; i++) {
+                sb.append(format);
             }
-            format.append("\r\nSupported post processors for output")
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "h <arg>",
+            sb.append("\r\nSupported post processors for output")
+                    .append(String.format(Locale.ENGLISH, format, "h <arg>",
                             "Highlights <arg> in response output of command execution"))
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "", "Example usage: help | h exit"))
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "m <emailId>",
+                    .append(String.format(Locale.ENGLISH, format, "", "Example usage: help | h exit"))
+                    .append(String.format(Locale.ENGLISH, format, "m <emailId>",
                             "Send response output of command execution to <emailId>"))
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "", "Example usage: help | m bob@hope.com"));
-            verifyResponse(is, String.format(Locale.ENGLISH, format.toString(), "dummy", "dummy description",
-                    "endpoint", "Invoke actuator endpoints", "exit", "Exit shell", "help", "Show list of help commands",
-                    "iae", "throws IAE", "test", "test description"));
+                    .append(String.format(Locale.ENGLISH, format, "", "Example usage: help | m bob@hope.com"));
+            verifyResponse(is, String.format(Locale.ENGLISH, sb.toString(), "auditEvents", "Event auditing",
+                    "autoConfigurationReport", "Autoconfiguration report", "beans", "List beans",
+                    "configurationPropertiesReport", "Configuration properties report", "dummy", "dummy description",
+                    "environment", "Environment details", "exit", "Exit shell", "health", "System health info", "help",
+                    "Show list of help commands", "iae", "throws IAE", "info", "System status", "loggers",
+                    "Logging configuration", "metrics", "Metrics operations", "requestMapping",
+                    "Request mapping information", "shutdown", "Shutdown application", "status", "System status",
+                    "test", "test description", "threadDump", "Print thread dump", "traces", "Trace information"));
         });
     }
 
@@ -123,64 +130,7 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
     public void testInteractive() throws JSchException, IOException {
         sshCall((is, os) -> {
             write(os, "test interactive", "anand");
-            verifyResponse(is, "Name: anandHi anand");
-        });
-    }
-
-    @Test
-    public void testEndpointList() throws JSchException, IOException {
-        sshCall((is, os) -> {
-            write(os, "endpoint list");
-            verifyResponse(is, getEndpointList());
-        });
-    }
-
-    private String getEndpointList() {
-        String[] endpoints = {"autoconfig", "beans", "configprops", "dump", "env", "health", "info", "loggers",
-            "metrics", "trace", "shutdown"};
-        String[] enabled = {"true", "true", "true", "true", "true", "true", "true", "true", "true", "true", "false"};
-        StringBuilder sb = new StringBuilder(String.format(Locale.ENGLISH, "%-16s%s\r\n", "Endpoints",
-                "Is Enabled?")).append("---------       -----------");
-        assertEquals(endpoints.length, enabled.length);
-        for (int i = 0; i < endpoints.length; i++) {
-            sb.append(String.format(Locale.ENGLISH, "\r\n%-16s%s", endpoints[i], enabled[i]));
-        }
-        return sb.toString();
-    }
-
-    @Test
-    public void testEndpointNullArg() throws JSchException, IOException {
-        sshCall((is, os) -> {
-            write(os, "endpoint invoke");
-            String response = "Null or unknown endpoint\r\n" + getEndpointList()
-                    + "\r\nUsage: endpoint invoke <endpoint>";
-            verifyResponse(is, response);
-        });
-    }
-
-    @Test
-    public void testEndpointInvalid() throws JSchException, IOException {
-        sshCall((is, os) -> {
-            write(os, "endpoint invoke invalid");
-            String response = "Null or unknown endpoint\r\n" + getEndpointList()
-                    + "\r\nUsage: endpoint invoke <endpoint>";
-            verifyResponse(is, response);
-        });
-    }
-
-    @Test
-    public void testEndpointDisabled() throws JSchException, IOException {
-        sshCall((is, os) -> {
-            write(os, "endpoint invoke shutdown");
-            verifyResponse(is, "Endpoint shutdown is not enabled");
-        });
-    }
-
-    @Test
-    public void testEndpointInvokeSuccess() throws JSchException, IOException {
-        sshCall((is, os) -> {
-            write(os, "endpoint invoke info");
-            verifyResponse(is, "{ }");
+            verifyResponse(is, "Name: anand{\r\n  \"obj\" : \"anand\"\r\n}\r\nHi anand");
         });
     }
 
@@ -213,7 +163,7 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
             }
         });
     }
-    
+
     @Test
     public void testMailProcessorFail() throws JSchException, IOException {
         sshCall((is, os) -> {
@@ -221,28 +171,34 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
             verifyResponse(is, "Error sending mail, please check logs for more info");
         });
     }
-    
+
     @Test
     public void testHighlightProcessor() throws JSchException, IOException {
         sshCall((is, os) -> {
+            String format = "\r\n%-35s%s"; // must be same with usageInfoFormat in SshdShellProperties.java
             write(os, "help | h <emailId>");
-            StringBuilder format = new StringBuilder("Supported Commands");
-            for (int i = 0; i < 6; i++) {
-                format.append("\r\n%-20s%s");
+            StringBuilder sb = new StringBuilder("Supported Commands");
+            for (int i = 0; i < 19; i++) {
+                sb.append(format);
             }
-            format.append("\r\nSupported post processors for output")
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "h <arg>",
+            sb.append("\r\nSupported post processors for output")
+                    .append(String.format(Locale.ENGLISH, format, "h <arg>",
                             "Highlights <arg> in response output of command execution"))
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "", "Example usage: help | h exit"))
-                    .append(String.format(Locale.ENGLISH, "\r\n%-29s%s", "m \u001B[43m<emailId>\u001B[0m",
+                    .append(String.format(Locale.ENGLISH, format, "", "Example usage: help | h exit"))
+                    .append(String.format(Locale.ENGLISH, "\r\n%-44s%s", "m \u001B[43m<emailId>\u001B[0m",
                             "Send response output of command execution to \u001B[43m<emailId>\u001B[0m"))
-                    .append(String.format(Locale.ENGLISH, "\r\n%-20s%s", "", "Example usage: help | m bob@hope.com"));
-            verifyResponse(is, String.format(Locale.ENGLISH, format.toString(), "dummy", "dummy description",
-                    "endpoint", "Invoke actuator endpoints", "exit", "Exit shell", "help", "Show list of help commands",
-                    "iae", "throws IAE", "test", "test description"));
+                    .append(String.format(Locale.ENGLISH, format, "", "Example usage: help | m bob@hope.com"));
+            verifyResponse(is, String.format(Locale.ENGLISH, sb.toString(), "auditEvents", "Event auditing",
+                    "autoConfigurationReport", "Autoconfiguration report", "beans", "List beans",
+                    "configurationPropertiesReport", "Configuration properties report", "dummy", "dummy description",
+                    "environment", "Environment details", "exit", "Exit shell", "health", "System health info", "help",
+                    "Show list of help commands", "iae", "throws IAE", "info", "System status", "loggers",
+                    "Logging configuration", "metrics", "Metrics operations", "requestMapping",
+                    "Request mapping information", "shutdown", "Shutdown application", "status", "System status",
+                    "test", "test description", "threadDump", "Print thread dump", "traces", "Trace information"));
         });
     }
-    
+
     @Test
     public void testInvalidCommand() throws JSchException, IOException {
         sshCall((is, os) -> {
@@ -250,7 +206,7 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
             verifyResponse(is, "Invalid command");
         });
     }
-    
+
     @Test
     public void testInvalidCommand2() throws JSchException, IOException {
         sshCall((is, os) -> {
@@ -258,4 +214,221 @@ public class SshdShellAutoConfigurationTest extends AbstractSshSupport {
             verifyResponse(is, "Invalid command");
         });
     }
- }
+
+    @Test
+    public void testAuditEventsCommandNullArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "auditEvents list");
+            verifyResponse(is, "Usage: auditEvents list {\"principal\":\"<user>\",\"after\":\"<yyyy-MM-dd HH:mm>\","
+                    + "\"type\":\"<type>\"}");
+        });
+    }
+    
+    @Test
+    public void testAuditEventsCommandInvalidJson() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "auditEvents list xxx");
+            verifyResponse(is, "Expected valid json as argument");
+        });
+    }
+    
+    @Test
+    public void testAuditEventsCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "auditEvents list {}");
+            verifyResponse(is, "{\r\n  \"events\" : [ ]\r\n}");
+        });
+    }
+    
+    @Test
+    public void testAutoConfigurationReportCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "autoConfigurationReport");
+            verifyResponse(is, "{\r\n  \"positiveMatches\" : {");
+        });
+    }
+    
+    @Test
+    public void testBeansCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "beans");
+            verifyResponse(is, "{\r\n  \"id\" : ");
+        });
+    }
+    
+    @Test
+    public void testConfigurationPropertiesReportCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "configurationPropertiesReport");
+            verifyResponse(is, "{\r\n  \"beans\" : {");
+        });
+    }
+    
+    @Test
+    public void testEnvironmentPatternCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+           write(os, "environment pattern");
+           verifyResponse(is, "{\r\n  \"activeProfiles\"");
+        });
+    }
+    
+    @Test
+    public void testEnvironmentEntryCommandValidArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "environment entry server.port");
+            verifyResponse(is, "{\r\n  \"activeProfiles\"");
+        });
+    }
+    
+    @Test
+    public void testEnvironmentEntryCommandInvalidArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "environment entry");
+            verifyResponse(is, "Usage: environment entry <stringToMatch>");
+        });
+    }
+    
+    @Test
+    public void testHealthCommand() throws JSchException, IOException {
+        int smtpPort = SocketUtils.findAvailableTcpPort();
+        ServerSetup setup = new ServerSetup(smtpPort, null, ServerSetup.PROTOCOL_SMTP);
+        setup.setServerStartupTimeout(5000);
+        GreenMail mailServer = new GreenMail(setup);
+        mailServer.start();
+        ((JavaMailSenderImpl) mailSender).setPort(smtpPort);
+        sshCall((is, os) -> {
+            write(os, "health");
+            verifyResponse(is, "{\r\n  \"status\" : \"UP\"");
+            mailServer.stop();
+        });
+    }
+    
+    @Test
+    public void testInfoCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "info");
+            verifyResponse(is, "{ }");
+        });
+    }
+    
+    @Test
+    public void testLoggersCommandInfo() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "loggers info");
+            verifyResponse(is, "{\r\n  \"levels\" : [ \"OFF\", \"ERROR\", \"WARN\", \"INFO\", \"DEBUG\", \"TRACE\" ]");
+        });
+    }
+    
+    @Test
+    public void testLoggersCommandLoggerLevelsNullArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "loggers level");
+            verifyResponse(is, "Usage: loggers level <loggerName>");
+        });
+    }
+    
+    @Test
+    public void testLoggersCommandLoggerLevelsValidArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "loggers level ROOT");
+            verifyResponse(is, "{\r\n  \"configuredLevel\" : \"INFO\",\r\n  \"effectiveLevel\" : \"INFO\"\r\n}");
+        });
+    }
+    
+    @Test
+    public void testLoggersCommandConfigureNullArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "loggers configure");
+            verifyResponse(is, "Usage: loggers configure {\"name\":\"<loggerName>\",\"configuredLevel\":"
+                    + "\"<Select from TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF>\"}");
+        });
+    }
+    
+    @Test
+    public void testLoggersCommandConfigureValidArg() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "loggers configure {\"name\":\"ROOT\",\"configuredLevel\":\"INFO\"}");
+            verifyResponse(is, "Changed log level for ROOT to INFO");
+        });
+    }
+    
+    @Test
+    public void testLoggersCommandConfigureInvalidJson() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "loggers configure {}");
+            verifyResponse(is, "Expected valid json as argument");
+        });
+    }
+    
+    @Test
+    public void testMetricsCommandListNames() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "metrics listNames");
+            verifyResponse(is, "{\r\n  \"names\" : [");
+        });
+    }
+    
+    @Test
+    public void testMetricsCommandNullMetricName() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "metrics metricName");
+            verifyResponse(is, "Usage: metrics metricName <metricName>");
+        });
+    }
+    
+    @Test
+    public void testMetricsCommandValidMetricName() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "metrics metricName jvm.memory.used");
+            verifyResponse(is, "{\r\n  \"jvmMemoryUsed");
+        });
+    }
+    
+    @Test
+    public void testRequestMappingCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "requestMapping");
+            verifyResponse(is, "{\r\n  \"/webjars/**\"");
+        });
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testShutdownCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "shutdown");
+            verifyResponse(is, "{\r\n  \"message\" : \"Shutting down, bye...\"\r\n}");
+        });
+    }
+    
+    @Test
+    public void testStatusCommand() throws JSchException, IOException {
+        int smtpPort = SocketUtils.findAvailableTcpPort();
+        ServerSetup setup = new ServerSetup(smtpPort, null, ServerSetup.PROTOCOL_SMTP);
+        setup.setServerStartupTimeout(5000);
+        GreenMail mailServer = new GreenMail(setup);
+        mailServer.start();
+        ((JavaMailSenderImpl) mailSender).setPort(smtpPort);
+        sshCall((is, os) -> {
+            write(os, "status");
+            verifyResponse(is, "{\r\n  \"status\" : \"UP\"\r\n}");
+            mailServer.stop();
+        });
+    }
+    
+    @Test
+    public void testThreadDumpCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "threadDump");
+            verifyResponse(is, "{\r\n  \"threads\" : [ {");
+        });
+    }
+    
+    @Test
+    public void testTracesCommand() throws JSchException, IOException {
+        sshCall((is, os) -> {
+            write(os, "traces");
+            verifyResponse(is, "{\r\n  \"traces\" : [ ]\r\n}");
+        });
+    }
+}
