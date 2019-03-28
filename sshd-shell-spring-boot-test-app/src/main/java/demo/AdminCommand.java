4e5a2c51c0369b78e1fc055950466004398ee097
anand <anand@localhost>
2019-03-28 20:46:33 +0800

    Updated demo methods

--- sshd-shell-spring-boot-test-app/src/main/java/demo/AdminCommand.java
+++ sshd-shell-spring-boot-test-app/src/main/java/demo/AdminCommand.java
@@ -16,6 +16,7 @@
 package demo;
 
 import org.springframework.stereotype.Component;
+import sshd.shell.springboot.ShellException;
 import sshd.shell.springboot.autoconfiguration.SshdShellCommand;
 
 /**
@@ -26,9 +27,14 @@ import sshd.shell.springboot.autoconfiguration.SshdShellCommand;
 @SshdShellCommand(value = "admin", description = "Admin functionality. Type 'admin' for supported subcommands",
         roles = "ADMIN")
 public class AdminCommand {
-    
+
     @SshdShellCommand(value = "manage", description = "Manage task. Usage: admin manage <arg>", roles = "ADMIN")
     public String manage(String arg) {
         return arg + " has been managed by admin";
+    }
+
+    @SshdShellCommand(value = "testShellException", description = "Test throwing ShellException")
+    public String testShellException(String arg) throws ShellException {
+        throw new ShellException("Exception Message:" + arg);
     }
 }
