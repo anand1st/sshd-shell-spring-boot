5c2f2d9faacad09ad426326a9e89de0defd6b5c3
Nithyanandan Natchimuthu <anand1st@gmail.com>
2019-10-31 15:36:20 +0800

    Sample files for test app. Added embedded redis support

--- sshd-shell-spring-boot-test-app/src/main/java/demo/Main.java
+++ sshd-shell-spring-boot-test-app/src/main/java/demo/Main.java
@@ -1,10 +1,13 @@
 package demo;
 
+import javax.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.util.SocketUtils;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import redis.embedded.RedisServer;
 
 /**
  *
@@ -14,8 +17,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @SpringBootApplication
 public class Main extends WebMvcConfigurationSupport {
 
+    private static final RedisServer REDIS_SERVER;
+
+    static {
+        int availablePort = SocketUtils.findAvailableTcpPort();
+        System.setProperty("embeddedRedisPort", String.valueOf(availablePort));
+        REDIS_SERVER = new RedisServer(availablePort);
+        REDIS_SERVER.start();
+    }
+
     public static void main(String... args) throws InterruptedException {
         SpringApplication.run(Main.class, args);
+    }
+
+    @PreDestroy
+    void cleanUp() {
+        REDIS_SERVER.stop();
     }
 
     @Override
---
+++ sshd-shell-spring-boot-test-app/src/main/resources/db/migration/test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `employee` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(20),
+    `email` varchar(50),
+    `date_of_birth` timestamp
+);
