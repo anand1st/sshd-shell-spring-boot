6f764f40e925fcb4c1e11cbe04cf7966d711e73c
Nithyanandan Natchimuthu <anand1st@gmail.com>
2022-06-15 21:57:43 +0800

    Updated code

--- sshd-shell-spring-boot-test-app/src/main/java/demo/MySecurityConfig.java
+++ sshd-shell-spring-boot-test-app/src/main/java/demo/MySecurityConfig.java
@@ -3,31 +3,25 @@ package demo;
 import java.util.Collection;
 import java.util.HashSet;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  *
  * @author anand
  */
-@Configuration
 @EnableWebSecurity
-public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+class MySecurityConfig {
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsServiceBean() {
+    private UserDetailsService userDetailsService() {
         return username -> new UserDetails() {
             private static final long serialVersionUID = 1L;
 
@@ -73,27 +67,20 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         };
     }
 
-    @SuppressWarnings("deprecation")
     @Bean
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsServiceBean());
+        authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return authProvider;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        SimpleUrlAuthenticationSuccessHandler authSuccessHandler = new SimpleUrlAuthenticationSuccessHandler();
-        authSuccessHandler.setUseReferer(true);
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().defaultSuccessUrl("/test", true);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
+        return http.build();
     }
 }
